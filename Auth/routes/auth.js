4a3391ada46b551be1ae76e39364372531071f12
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-11-25 21:04:39 -0500

    Failed at trying to get validation with valid emails and passwords, set up a function to see if the email has already been registered

--- Auth/routes/auth.js
+++ Auth/routes/auth.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+// Validation
+const Joi = require("@hapi/joi");
+
+const schema = {
+  name: Joi.string()
+    .min(6)
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(6)
+    .required()
+};
+
 router.post("/register", async (req, res) => {
+  // Validate the data before new user
+  //   const { error } = schema.validate(req.body, schema);
+  //   if (error) return res.status(400).send(error.details[0].message);
+
+  // Checking if user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email is already registered!");
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
